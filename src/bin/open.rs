477f47e77a994113e4099d4c6d239ad154a9770a
Omer Yacine <mariocynicys@gmail.com>
2022-12-01 02:14:59 +0000

    fix linting issue

--- src/bin/open.rs
+++ src/bin/open.rs
@@ -6,7 +6,7 @@ fn main() {
     } else if let Ok(e) = env::var("EDITOR") {
         e
     } else {
-        "nano".to_string()
+        "code".to_string()
     };
 
     let problem: u32 = pico_args::Arguments::from_env().free_from_str().expect("What problem should be opened?");
@@ -14,5 +14,5 @@ fn main() {
     process::Command::new(&editor)
         .arg(format!("src/bin/{:02}.rs", problem))
         .spawn()
-        .expect(&format!("Failed to execute {}", editor));
+        .unwrap_or_else(|_| panic!("Failed to execute {}", editor));
 }
